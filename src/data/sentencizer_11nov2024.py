84eb88302507ca7e8ae7a86e22eeea2bfe066b87
patterson-s <33936562+patterson-s@users.noreply.github.com>
2024-11-11 12:51:55 -0500

    fixed sentencizer in model-v4

--- src/data/sentencizer_11nov2024.py
+++ src/data/sentencizer_11nov2024.py
@@ -1,11 +1,13 @@
 import spacy
 
 # Load the existing model
-nlp = spacy.load("C:/Users/spatt/Desktop/diss_3/state_ner/models/model-last")
+#nlp = spacy.load("C:/Users/spatt/Desktop/diss_3/state_ner/models/model-last")
+nlp = spacy.load("C:/Users/spatt/Desktop/diss_3/state_ner/models/model-v4")
 
 # Add a sentencizer if it's not already in the pipeline
 if "sentencizer" not in nlp.pipe_names:
     nlp.add_pipe("sentencizer", before="ner")
 
 # Save the updated model with sentence boundaries enabled
-nlp.to_disk("C:/Users/spatt/Desktop/diss_3/state_ner/models/model-last-sentencizer")
+#nlp.to_disk("C:/Users/spatt/Desktop/diss_3/state_ner/models/model-last-sentencizer")
+nlp.to_disk("C:/Users/spatt/Desktop/diss_3/state_ner/models/model-v4")
